5f33ce857911015a02cdb2c53149be407daf496b
vitekkor <viktor.korotkikh@gmail.com>
2021-02-15 19:20:26 +0300

    to table fun

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from matplotlib import pyplot, ticker
 # math
 import numpy
+import pandas
 import lagrange
 
 points = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
@@ -37,12 +38,29 @@ def to_plot(fun, a: float, b: float, title: str, count: int = 1000, major_xaxis:
     pyplot.show()
 
 
-def to_table(fun, a: float, b: float, title: str):
-    x, y = pick(fun, a, b, 1000)
+def to_table(fun, a: float, b: float, title: str, count: int = 10):
+    fig, ax = pyplot.subplots()
+
+    # hide axes
+    ax.axis('off')
+    ax.axis('tight')
+
+    x, y = pick(fun, a, b, count)
+    x = x.tolist()
+    y = y.tolist()
+    data = []
+    for xx in x:
+        data.append([xx, y[x.index(xx)]])
+
+    df = pandas.DataFrame(data, columns=list('xy'))
+
+    ax.table(cellText=df.values, colLabels=df.columns, loc='center')
+
     pyplot.title(title)
-    pyplot.plot(x, y, 'b-')
+    fig.tight_layout()
     pyplot.show()
 
 
 if __name__ == '__main__':
     to_plot(lambda points_to_calc: lagrange.lagrange(function, points, points_to_calc), 0.05, 0.95, "lagrange")
+    to_table(lambda points_to_calc: lagrange.lagrange(function, points, points_to_calc), 0.05, 0.95, "lagrange")
